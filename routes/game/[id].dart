661cbd52a83c3ef611cce50ecc9f6a1b89e24384
Schamppu <41271610+Schamppu@users.noreply.github.com>
2023-12-09 22:16:45 +0200

    Added a dynamic game state websocket endpoint

--- routes/game/[id].dart
+++ routes/game/[id].dart
@@ -1,15 +1,15 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
 
-Future<Response> onRequest(RequestContext context) async {
+Future<Response> onRequest(RequestContext context, String id) async {
   final handler = webSocketHandler((channel, protocol) {
-    // Listen to clients
+    // Here we want to receive the stones that are being thrown.
     channel.stream.listen((message) {
       print(message);
     });
 
-    // Send a msg to client
-    channel.sink.add('hi');
+    // Here we want to send the game state.
+    channel.sink.add('hi, this is lobby $id');
   });
   return handler(context);
 }
