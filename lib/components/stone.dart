35c90024ca94c823e82d5ce9c7b6fd0972766c47
Schamppu <41271610+Schamppu@users.noreply.github.com>
2023-12-10 04:46:56 +0200

    Tried to do some fixes

--- lib/components/stone.dart
+++ lib/components/stone.dart
@@ -5,8 +5,7 @@ import 'package:uuid/uuid.dart';
 /// Curling stone object
 class Stone {
   /// Constructor
-  Stone({required this.x, required this.y, required this.user})
-      : id = Uuid().v4();
+  Stone({required this.x, required this.y, required this.user}) : id = Uuid().v4();
 
   final String? id;
   double? x;
@@ -16,11 +15,13 @@ class Stone {
   static const double mass = 19.96;
   double angle = 0.0;
   double speed = 0.0;
+  bool started = false;
+  double speedX = 0.0;
+  double speedY = 0.0;
 
   /// helper function to check if colliding with another stone
   bool isCollidingWith(Stone otherStone) {
-    double distanceSquared = (x! - otherStone.x!) * (x! - otherStone.x!) +
-        (y! - otherStone.y!) * (y! - otherStone.y!);
+    double distanceSquared = (x! - otherStone.x!) * (x! - otherStone.x!) + (y! - otherStone.y!) * (y! - otherStone.y!);
     double minDistanceSquared = (2 * radius) * (2 * radius);
     return distanceSquared < minDistanceSquared;
   }
@@ -28,6 +29,14 @@ class Stone {
   void slide(double angle, double speed) {
     this.angle = angle;
     this.speed = speed;
+    double radians = angle * (pi / 180);
+    if (!started) {
+      started = true;
+      // Nopeus x-suunnassa
+      speedX = speed * cos(radians);
+      // Nopeus y-suunnassa
+      speedY = speed * sin(radians);
+    }
   }
 
   void update(double deltaTime) {
@@ -35,27 +44,25 @@ class Stone {
     // Lasketaan vain jarrutusta.
     // Kulmien nopeuksien jälkeen laske kitkan vaikutus X- ja Y-nopeuksiin ajan
     // funktiona.
-    double radians = angle * (pi / 180);
-
-    // Nopeus x-suunnassa
-    double speedX = speed * cos(radians);
-    // Nopeus y-suunnassa
-    double speedY = speed * sin(radians);
 
     // Drags. FIXME
-    double dragX = 0.5 * 0.02 * pow(speedX, 2) / mass;
-    double dragY = 0.5 * 0.02 * pow(speedY, 2) / mass;
+    double dragX = 0.5 * 0.02 * pow(speedX, 2);
+    double dragY = 0.5 * 0.02 * pow(speedY, 2);
 
     // Speed reductions based on drag and mass
     speedX -= dragX;
     speedY -= dragY;
 
-    if (speedX.abs() < 0.01) {
+    if (speedX.abs() < 1) {
       speedX = 0.0;
     }
 
-    if (speedY.abs() < 0.01) {
+    if (speedY.abs() < 1) {
       speedY = 0.0;
+    }
+
+    if (speedX == 0.0 && speedY == 0.0) {
+      speed = 0.0;
     }
 
     // TODO: Laske uusien koordinaattien pysyminen koordinaatistossa
